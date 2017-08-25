7fdcdda9b4ffd1d026b4e667c8b3e19da9043658
Georg Sawtschuk <georg.the.moon@googlemail.com>
2017-08-25 14:11:16 +0200

    добавлен контроль апвотов

--- src/voteprediction.js
+++ src/voteprediction.js
@@ -6,6 +6,37 @@ const global = require("./global");
 
 const MAXAGE = (1000 * 60 * 10) / 3;
 
+class Stat {
+    constructor(userid, quote) {
+        this.userid = userid;
+        this.quote = quote;
+        this.votes = [];
+    }
+
+    cleanupVotes() {
+        if(this.votes.length > 0) {
+            log.info("oldest vote's age " + ((Date.now() - this.votes[0]) / 1000 / 60 / 60).toFixed(2) + " hours ");
+        }
+        while(this.votes.length > 0 && this.votes[0] < (Date.now() - (1000*60*60*24))) {
+            this.votes.shift();
+            log.info("del old vote from " + this.userid + " cnt " + this.votes.length);
+        }
+    }
+
+    checkVote() {
+        this.cleanupVotes();
+        return (this.votes.length < this.quote);
+    }
+
+    addVote() {
+        this.votes.push(Date.now());
+        log.info("added vote to " + this.userid + " cnt " + this.votes.length);
+    }
+}
+
+const STATS = {}
+let VOTED = false;
+
 function notVoted(content, userid, vote) {
     for(v of content.active_votes) {
         
@@ -15,11 +46,11 @@ function notVoted(content, userid, vote) {
                 log.info("\t" + userid + " already voted with same weight");
                 return false;
             } else {
-                log.info(userid + " not yet voted" );
                 break;
             }
         }
     }
+    log.info(userid + " not yet voted" );
     return true;    
 }
 
@@ -33,7 +64,7 @@ async function doVote(vote, userid) {
 async function followVote(vote) {
     log.info("follow vote " + vote.author + "/" + vote.permlink);
     let content = await steem.api.getContentAsync(vote.author, vote.permlink);
-    
+    vote.weight == 10000;
     for(let userid of Object.keys(global.settings.users)) {
         if(notVoted(content, userid, vote)) {
             await doVote(vote, userid);
@@ -46,14 +77,18 @@ async function processBlock(bn) {
         let transactions = await steem.api.getOpsInBlockAsync(bn, false);
         //log.debug(JSON.stringify(transactions));
         for(let tr of transactions) {
-            log.debug("tr " + tr.trx_in_block);
+            log.trace("tr " + tr.trx_in_block);
             let op = tr.op[0];
             let opBody = tr.op[1];
             switch(op) {
                 case "vote":
-                    log.debug("found vote of " + opBody.voter + " for " + opBody.author + "/" + opBody.permlink);
                     if(global.settings.leaders.includes(opBody.voter)) {
-                        await followVote(opBody);
+                        log.debug("found vote of " + opBody.voter + " for " + opBody.author + "/" + opBody.permlink);
+                        if(opBody.weight > 0 && STATS[opBody.voter].checkVote()) {
+                            await followVote(opBody);
+                            STATS[opBody.voter].addVote();
+                            VOTED = true;
+                        }
                     }
                     //break;
             }
@@ -67,6 +102,12 @@ async function processBlock(bn) {
  */
 module.exports.run = async function() {
 
+    //FILL Stats
+ 
+    for(let u of global.settings.leaders) {
+        STATS[u] = new Stat(u, Math.floor(30 / global.settings.leaders.length));
+    }
+
     let props = await golos.getCurrentServerTimeAndBlock();
     let block = props.block - 3;
     //block = 7780489;
@@ -79,7 +120,16 @@ module.exports.run = async function() {
                 await sleep(12000);
                 continue;
             }
+            VOTED = false;
             await processBlock(block++);
+            if(VOTED) {
+                log.info("***************************************")
+                let leaders = Object.keys(STATS);
+                leaders.sort();
+                for(let u of leaders) {
+                    log.info(u + " votes last 24h : " + STATS[u].votes.length);
+                }
+            }
         } catch(e) {
             log.error("Error catched in main loop!");
             log.error(golos.getExceptionCause(e));            
